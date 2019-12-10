fcba9632770ab6871b05e7b8c53fc2f4814e2f79
hyunchul <byunhyunchul@naver.com>
2019-12-10 14:53:11 +0000

    201912102353

--- cam.py
+++ cam.py
@@ -1,47 +1,50 @@
-import time
-import cv2
-import sys
-import numpy as np
+# import the necessary packages
 from picamera.array import PiRGBArray
 from picamera import PiCamera
+import time
+import cv2
 
-# camera settings
+def detect(img, cascade):
+    rects = cascade.detectMultiScale(img, scaleFactor=1.3, minNeighbors=4, minSize=(30, 30),
+                                     flags=cv2.CASCADE_SCALE_IMAGE)
+    if len(rects) == 0:
+        return []
+    rects[:,2:] += rects[:,:2]
+    return rects
+
+def draw_rects(img, rects, color):
+    for x1, y1, x2, y2 in rects:
+        cv2.rectangle(img, (x1, y1), (x2, y2), color, 2)
+
+# initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
 camera.resolution = (640, 480)
-rawCapture = PiRGBArray(camera, size=(640,480))
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(640, 480))
+cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
-time.sleep(1)
+# allow the camera to warmup
+time.sleep(0.1)
 
-# video input 
-faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
-
-# capture frame from the camera
+# capture frames from the camera
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    # grab the raw NumPy array representing the image, then initialize the timestamp
+    # and occupied/unoccupied text
+    img = frame.array
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.equalizeHist(gray)
 
-    image = frame.array
+    rects = detect(gray, cascade)
+    vis = img.copy()
+    draw_rects(vis, rects, (0, 255, 0))
 
-# face detection
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # show the frame
+    cv2.imshow("Frame", vis)
+    key = cv2.waitKey(1) & 0xFF
 
-faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30),
-        flags=cv2.CASCADE_SCALE_IMAGE
-    )
+    # clear the stream in preparation for the next frame
+    rawCapture.truncate(0)
 
-# Draw a rectangle around the faces
-for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-#show the frames
-
-        cv2.imshow("Frame", image)
-        key = cv2.waitKey(1) & 0xFF
-
-        rawCapture.truncate(0)
-
-        if key == ord("q"):
-            break
+        # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        break
